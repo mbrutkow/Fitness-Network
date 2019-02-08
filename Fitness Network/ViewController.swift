b90d6509b5f265651a68e84c02230374d94701a6
Michael Rutkowski <mbr@Soonersofts-MacBook-Pro.local>
2019-02-08 11:03:27 -0500

    Initial Commit

---
+++ Fitness Network/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fitness Network
+//
+//  Created by Michael Rutkowski on 2/8/19.
+//  Copyright © 2019 Michael Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
